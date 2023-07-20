ddaa53d25b5fbe479ca8cc6b75520855b0bf2c31
Roman <email>
2023-07-20 15:18:47 +0600

    rm svc

--- src/pages/api/connection_details.ts
+++ src/pages/api/connection_details.ts
@@ -1,10 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import livekitServer, {
-  AccessToken,
-  RoomServiceClient,
-} from "@dtelecom/server-sdk-js";
-import { CharacterName } from "@/components/CharacterSelector";
+import type {NextApiRequest, NextApiResponse} from "next";
+import {AccessToken,} from "@dtelecom/server-sdk-js";
+import {CharacterName} from "@/components/CharacterSelector";
 import {generateUUID} from "@/util/generateUUID";
 
 export type ConnectionDetailsBody = {
@@ -27,7 +24,7 @@ export default async function handler(
   res: NextApiResponse<ConnectionDetails | ErrorResponse>
 ) {
   if (req.method !== "POST") {
-    return res.status(400).json({ error: "Invalid method" });
+    return res.status(400).json({error: "Invalid method"});
   }
 
   const {
@@ -39,34 +36,23 @@ export default async function handler(
   const apiSecret = process.env.API_SECRET;
 
   if (!apiKey || !apiSecret) {
-    return res.status(500).json({ error: "Server misconfigured 1" });
+    return res.status(500).json({error: "Server misconfigured 1"});
   }
 
-  if (!username) return res.status(400).json({ error: "Missing username" });
-  if (!character) return res.status(400).json({ error: "Missing character" });
-  if (!room) return res.status(400).json({ error: "Missing room_name" });
+  if (!username) return res.status(400).json({error: "Missing username"});
+  if (!character) return res.status(400).json({error: "Missing character"});
+  if (!room) return res.status(400).json({error: "Missing room_name"});
 
   const token = new AccessToken(
     apiKey,
     apiSecret,
-    { identity: generateUUID(), name: username }
+    {identity: generateUUID(), name: username}
   );
 
   const wsUrl = await token.getWsUrl();
 
   if (!wsUrl) {
-    return res.status(500).json({ error: "Server misconfigured, ws url" });
-  }
-
-  const livekitHost = wsUrl?.replace("wss://", "https://");
-
-  const roomService = new RoomServiceClient(livekitHost, apiKey, apiSecret);
-
-  try {
-    await roomService.getParticipant(room, username);
-    return res.status(401).json({ error: "Username already exists in room" });
-  } catch {
-    // If participant doesn't exist, we can continue
+    return res.status(500).json({error: "Server misconfigured, ws url"});
   }
 
   token.addGrant({
@@ -75,6 +61,6 @@ export default async function handler(
     canPublish: true,
     canPublishData: true,
   });
-  token.metadata = JSON.stringify({ character });
-  res.status(200).json({ token: token.toJwt(), ws_url: wsUrl });
+  token.metadata = JSON.stringify({character});
+  res.status(200).json({token: token.toJwt(), ws_url: wsUrl});
 }
